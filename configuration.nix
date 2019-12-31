a39bc0e9e9e04180ad11eb1395417d17904df2cd
LightDiscord <root@arnaud.sh>
2019-12-31 18:04:04 +0100

    feat: reformat

--- configuration.nix
+++ configuration.nix
@@ -1,150 +1,84 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+	imports = [
+		./hardware-configuration.nix
+	];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.extraModulePackages = with config.boot.kernelPackages; [ exfat-nofuse ];
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "ritsu"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+	boot.kernelPackages = pkgs.linuxPackages_latest;
+	boot.extraModulePackages = with config.boot.kernelPackages; [ exfat-nofuse ];
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp69s0.useDHCP = true;
-  # networking.useNetworkd = true;
+	boot.cleanTmpDir = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+	networking.hostName = "ritsu";
 
-  # Select internationalisation properties.
-  i18n = {
-    # consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "fr";
-    defaultLocale = "en_US.UTF-8";
-  };
+	networking.useDHCP = false;
+	networking.interfaces.enp69s0.useDHCP = true;
 
-  # Set your time zone.
-  time.timeZone = "Europe/Paris";
+	i18n = {
+		consoleKeyMap = "fr";
+		defaultLocale = "en_US.UTF-8";
+	};
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    wget vim
-  ];
+	time.timeZone = "Europe/Paris";
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+	environment.systemPackages = with pkgs; [
+		vim
+	];
 
-  # List services that you want to enable:
+	sound.enable = true;
+	hardware.pulseaudio = {
+		enable = true;
+		support32Bit = true;
+	};
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+	services.xserver = {
+		enable = true;
+		layout = "fr";
+		videoDrivers = [ "nvidia" ];
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+		displayManager.lightdm.enable = true;
+		windowManager.i3.enable = true;
+		libinput.enable = true;
+	};
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
 
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+	hardware.nvidia.optimus_prime = {
+		enable = true;
+		nvidiaBusId = "PCI:1:0:0";
+		intelBusId = "PCI:0:2:0";
+	};
 
-  # Enable the X11 windowing system.
-  services.xserver = {
-    enable = true;
-    layout = "fr";
-    deviceSection = ''
-      Option "Monitor-HDMI-0" "multihead1"
-      Option "Monitor-eDP-1-1" "multihead2"
-    '';
-    extraConfig = ''
-      Section "Monitor"
-        Identifier "multihead1"
-        Option "LeftOf" "eDP-1-1"
-      EndSection
+	hardware.nvidia.modesetting.enable = true;
 
-      Section "Monitor"
-        Identifier "multihead2"
-        Option "Primary" "true"
-      EndSection
-    '';
-    # xrandrHeads = [
-    #   { output = "HDMI-0"; }
-    #   { output = "eDP-1-1"; primary = true; }
-    # ];
-    # screenSection = ''
-    #   Monitor "multihead2"
-    #   Option "metamodes" "eDP-1-1: nvidia-auto-select +1920+0, HDMI-0: nvidia-auto-select +0+0"
-    # '';
-    # deviceSection = ''
-    #   Option "MetaModeOrientation" "HDMI-0 LeftOf eDP-1-1"
-    # '';
-  };
-  # services.xserver.xkbOptions = "eurosign:e";
+	hardware.opengl = {
+		enable = true;
+		driSupport32Bit = true;
+	};
 
-  # Enable touchpad support.
-  services.xserver.libinput.enable = true;
+	users.users.arnaud = {
+		isNormalUser = true;
+		extraGroups = [ "wheel" ];
+		packages = [ pkgs.steam ];
+	};
 
-  # Enable the KDE Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.windowManager.i3.enable = true;
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia.optimus_prime = {
-    enable = true;
-    nvidiaBusId = "PCI:1:0:0";
-    intelBusId = "PCI:0:2:0";
-  };
-  hardware.nvidia.modesetting.enable = true;
+	services.pcscd.enable = true;
 
-  # steam
-  hardware.opengl.driSupport32Bit = true;
-  hardware.pulseaudio.support32Bit = true;
-  hardware.opengl.enable = true;
+	services.udev.packages = with pkgs; [ yubikey-personalization libu2f-host ];
 
-  # services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.plasma5.enable = true;
-  
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.arnaud = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    packages = [ pkgs.steam ];
-  };
+	programs.gnupg.agent = {
+		enable = true;
+		enableSSHSupport = true;
+	};
 
-  services.pcscd.enable = true;
-  services.udev.packages = with pkgs; [ yubikey-personalization libu2f-host ];
+	nixpkgs.config.allowUnfree = true;
 
-  boot.cleanTmpDir = true;
-
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
-
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "19.09"; # Did you read the comment?
-  nixpkgs.config.allowUnfree = true;
+	# This value determines the NixOS release with which your system is to be
+	# compatible, in order to avoid breaking some software such as database
+	# servers. You should change this only after NixOS release notes say you
+	# should.
+	system.stateVersion = "19.09";
 }
